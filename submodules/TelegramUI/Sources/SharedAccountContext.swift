6322d79e7e0a4e445678c4b9dea618920ada6df2
Lenar Gilyazov <l.gilyazov@ya.ru>
2024-11-06 20:36:30 +0300

    Добавил вкладку новостей

--- submodules/TelegramUI/Sources/SharedAccountContext.swift
+++ submodules/TelegramUI/Sources/SharedAccountContext.swift
@@ -75,6 +75,8 @@ import GiftViewScreen
 import StarsIntroScreen
 import ContentReportScreen
 
+import TPNews
+
 private final class AccountUserInterfaceInUseContext {
     let subscribers = Bag<(Bool) -> Void>()
     let tokens = Bag<Void>()
@@ -1704,6 +1706,10 @@ public final class SharedAccountContextImpl: SharedAccountContext {
     
     public func makeChatListController(context: AccountContext, location: ChatListControllerLocation, controlsHistoryPreload: Bool, hideNetworkActivityStatus: Bool, previewing: Bool, enableDebugActions: Bool) -> ChatListController {
         return ChatListControllerImpl(context: context, location: location, controlsHistoryPreload: controlsHistoryPreload, hideNetworkActivityStatus: hideNetworkActivityStatus, previewing: previewing, enableDebugActions: enableDebugActions)
+    }
+    
+    public func makeNewsFeedController(context: AccountContext) -> ViewController {
+        return NewsFeedViewController(context: context)
     }
     
     public func makePeerSelectionController(_ params: PeerSelectionControllerParams) -> PeerSelectionController {
