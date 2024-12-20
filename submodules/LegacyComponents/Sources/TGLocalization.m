9726894e7055a5f2dd748d455e1bc275de850077
Lenar Gilyazov <l.gilyazov@ya.ru>
2024-12-20 21:39:22 +0300

    Updated app icon and strings

--- submodules/LegacyComponents/Sources/TGLocalization.m
+++ submodules/LegacyComponents/Sources/TGLocalization.m
@@ -55,9 +55,9 @@ static NSString *fallbackString(NSString *key, NSString *code) {
             appTitle = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
         }
         if (appTitle == nil) {
-            appTitle = @"Dal";
+            appTitle = @"Dahl";
         }
-        NSString *originalTitle = @"Dal";
+        NSString *originalTitle = @"Dahl";
         if (![appTitle isEqualToString:originalTitle]) {
             NSMutableDictionary *updatedDict = [[NSMutableDictionary alloc] initWithDictionary:dict];
             for (NSString *key in dict.keyEnumerator) {
