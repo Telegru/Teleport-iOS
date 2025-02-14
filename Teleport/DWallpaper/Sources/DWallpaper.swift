3652c00ed79a15dd35e093159cefe06122589870
Dinislam <dinis.ish@gmail.com>
2025-02-14 20:33:35 +0300

    DAHL-57 Add moscow

--- Teleport/DWallpaper/Sources/DWallpaper.swift
+++ Teleport/DWallpaper/Sources/DWallpaper.swift
@@ -7,7 +7,8 @@ public enum DWallpaper: CaseIterable {
     case kazan
     case russia
     case saintPetersburg
-    
+    case moscow
+
     public func makeWallpaper(darkMode: Bool = false) -> TelegramWallpaper? {
         let (resourceName, resourceType) = resourceData
         let (previewName, previewType) = previewData
@@ -111,9 +112,10 @@ public enum DWallpaper: CaseIterable {
     
     private var resourceData: (String, String) {
         switch self {
-        case .kazan:           return ("WallpaperKazan", "svg")
-        case .russia:          return ("WallpaperRussia", "svg")
-        case .saintPetersburg: return ("WallpaperSP", "svg")
+        case .kazan:            return ("WallpaperKazan", "svg")
+        case .russia:           return ("WallpaperRussia", "svg")
+        case .saintPetersburg:  return ("WallpaperSP", "svg")
+        case .moscow:           return ("WallpaperMoscow", "svg")
         }
     }
     
@@ -122,17 +124,20 @@ public enum DWallpaper: CaseIterable {
         case .kazan:           return ("WallpaperKazan", "png")
         case .russia:          return ("WallpaperRussia", "png")
         case .saintPetersburg: return ("WallpaperSP", "png")
+        case .moscow:          return ("WallpaperMoscow", "png")
         }
     }
     
     private var fileData: (Int64, Int64, Int64) {
         switch self {
         case .kazan:
-            return (331222124451, 123444, 1234)
+            return (231222124451, 123444, 1234)
         case .russia:
-            return (441222124562, 223555, 5678)
+            return (241222124562, 223555, 5678)
         case .saintPetersburg:
-            return (551222124673, 323666, 9876)
+            return (251222124673, 323666, 9876)
+        case .moscow:
+            return (223442223441, 533112, 3451)
         }
     }
     
@@ -144,6 +149,8 @@ public enum DWallpaper: CaseIterable {
             return [15377262, 15787142, 15900351, 15253614]
         case .saintPetersburg:
             return [14346955, 15518719, 15518719, 12182271]
+        case .moscow:
+            return [8372873, 15518719, 15900351, 15777914]
         }
     }
     
@@ -155,6 +162,8 @@ public enum DWallpaper: CaseIterable {
             return [9100274, 8949228, 14917610, 6790381]
         case .saintPetersburg:
             return [16696470, 9842623, 5200853, 16696470]
+        case .moscow:
+            return [8364929, 8949228, 14917610, 16696470]
         }
     }
 }
